2e636639f1b278732a14be5c462310fea6c0b1f5
Zorikto Baldanov <45837027+Paarkzb@users.noreply.github.com>
2024-09-02 22:13:32 +0800

    refactor request to yandex speller

--- pkg/service/note.go
+++ pkg/service/note.go
@@ -20,34 +20,11 @@ func NewNoteService(repo repository.Note) *NoteService {
 }
 
 func (s *NoteService) Create(userId int, note model.Note) (int, error) {
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-
-	client := &http.Client{Transport: tr}
-	req, err := http.NewRequest(http.MethodGet, "http://speller.yandex.net/services/spellservice.json/checkText", nil)
+	text, err := checkText(note.Body)
 	if err != nil {
 		return 0, err
 	}
-
-	q := req.URL.Query()
-	q.Add("text", note.Body)
-	req.URL.RawQuery = q.Encode()
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return 0, err
-	}
-	defer resp.Body.Close()
-
-	var sr []model.SpellerResponse
-	if err := json.NewDecoder(resp.Body).Decode(&sr); err != nil {
-		return 0, err
-	}
-
-	for _, v := range sr {
-		note.Body = strings.Replace(note.Body, v.Word, v.S[0], 1)
-	}
+	note.Body = text
 
 	return s.repo.Create(userId, note)
 }
@@ -69,6 +46,17 @@ func (s *NoteService) Update(userId, noteId int, input model.UpdateNoteInput) er
 		return err
 	}
 
+	text, err := checkText(*input.Body)
+	if err != nil {
+		return err
+	}
+	*input.Body = text
+
+	return s.repo.Update(userId, noteId, input)
+}
+
+func checkText(text string) (string, error) {
+
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
@@ -76,27 +64,27 @@ func (s *NoteService) Update(userId, noteId int, input model.UpdateNoteInput) er
 	client := &http.Client{Transport: tr}
 	req, err := http.NewRequest(http.MethodGet, "http://speller.yandex.net/services/spellservice.json/checkText", nil)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	q := req.URL.Query()
-	q.Add("text", *input.Body)
+	q.Add("text", text)
 	req.URL.RawQuery = q.Encode()
 
 	resp, err := client.Do(req)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer resp.Body.Close()
 
 	var sr []model.SpellerResponse
 	if err := json.NewDecoder(resp.Body).Decode(&sr); err != nil {
-		return err
+		return "", err
 	}
 
 	for _, v := range sr {
-		*input.Body = strings.Replace(*input.Body, v.Word, v.S[0], 1)
+		text = strings.Replace(text, v.Word, v.S[0], 1)
 	}
 
-	return s.repo.Update(userId, noteId, input)
+	return text, nil
 }
